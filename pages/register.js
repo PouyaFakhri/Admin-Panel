f979096ccfe37cd21e81236b0d478551c381a980
PouyaFakhri <pouyaf77.pf@gmail.com>
2025-06-05 13:52:37 +0330

    add register page and registerform styles

--- pages/register.js
+++ pages/register.js
@@ -1,11 +1,13 @@
+import styles from "../styles/registerpage.module.css";
+import RegisterForm from "../components/templates/RegisterForm";
 
 function Register() {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Admin Panel</h2>
-     
+      <RegisterForm />
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
